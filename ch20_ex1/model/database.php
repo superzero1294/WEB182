18d830c91f3c01b49614cc05e08d3701fb94a623
superzero1294 <45079026+superzero1294@users.noreply.github.com>
2018-11-16 18:02:05 -0500

    Create database.php

---
+++ ch20_ex1/model/database.php
@@ -0,0 +1,20 @@
+<?php
+$dsn = 'mysql:host=localhost;dbname=web182jacks';
+$username = 'njackson';
+$password = 'NiJac7614';
+$options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+
+try {
+    $db = new PDO($dsn, $username, $password, $options);
+} catch (PDOException $e) {
+    $error_message = $e->getMessage();
+    include 'errors/db_error_connect.php';
+    exit;
+}
+
+function display_db_error($error_message) {
+    global $app_path;
+    include 'errors/db_error.php';
+    exit;
+}
+?>
